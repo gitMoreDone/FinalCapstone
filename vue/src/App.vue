defef21dd784111da9404b227670b8762e28dcf0
Lucas Carle <lucascarle@gmail.com>
2024-12-02 12:27:13 -0500

    Lucas and Matt created Vue Components and filled in PlantSearch component. The route is established and minor format styling was added

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="capstone-app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'plantSearch' }">Search</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
+    <site-header />
     <router-view />
+    <site-footer />
   </div>
 </template>
+
+<script>
+import SiteHeader from './components/SiteHeader.vue';
+import SiteFooter from './components/SiteFooter.vue';
+
+
+</script>
---
+++ vue/src/views/PlantSearchView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <plant-search />
+    </div>
+</template>
+
+
+<script>
+import PlantSearch from '../components/PlantSearch.vue';
+
+export default {
+    components: {
+        PlantSearch
+    }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
