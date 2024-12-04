fb30bc7f7ba8bd941122be4b59b19c5517a352ab
Joshua Hardy <jhardybuis@gmail.com>
2024-12-04 13:05:35 -0500

    FixedBrandClick

--- vue/src/components/SiteHeader.vue
+++ vue/src/components/SiteHeader.vue
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-success bg-gradient">
   <div class="container-fluid">
 
-    <a class="navbar-brand" href="https://localhost:5173" id="brand">
+    <a class="navbar-brand" id="brand">
         <img src="https://res.cloudinary.com/dwdijh29x/image/upload/v1733323940/sprout-icon-light_wvq61u.png" width="60" height="60" class="d-inline-block align-text-bottom"/>
         Sprout</a>
 
