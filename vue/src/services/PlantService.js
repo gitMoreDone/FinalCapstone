d65a6c541b5616a9cfb35ad6e2b0d2b7cdfd6cc4
Lucas Carle <lucascarle@gmail.com>
2024-12-03 21:16:25 -0500

    Fixed issue with addPlants function

--- vue/src/services/PlantService.js
+++ vue/src/services/PlantService.js
@@ -1,27 +1,35 @@
 import axios from 'axios';
 
 const service = axios.create({
-  baseURL: import.meta.env.VITE_REMOTE_API}
+    baseURL: import.meta.env.VITE_REMOTE_API
+}
 );
 
 
 export default {
-    getPlantById(id){
-        
+    getPlantById(id) {
+
         return service.get(`/plants/${id}`);
     },
-    getPlants(){
+    getPlants() {
         const plants = service.get('/plants');
         return plants;
     },
-    getSavedPlants(userId, token){
+    getSavedPlants(userId, token) {
 
-    
-        const plants = service.get(`/garden/${userId}`, {headers: {Authorization: `Bearer ${token}`}});
+
+        const plants = service.get(`/garden/${userId}`, { headers: { Authorization: `Bearer ${token}` } });
         return plants;
     },
-    addPlant(plant){
-        
+    addPlant(plant) {
+        let currentToken = localStorage.getItem('token');
+        let currentUser = JSON.parse(localStorage.getItem('user'));
+
+        if (currentToken) {
+            // Set token axios requests
+            axios.defaults.headers.common['Authorization'] = `Bearer ${currentToken}`;
+        }
+
         return service.post('/garden', plant);
     }
 
