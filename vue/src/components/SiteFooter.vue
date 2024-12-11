04e99effd8f6e0d6237c6fc65d47570eebdd91c4
Eliza Adams <elizaa29.ea@gmail.com>
2024-12-11 15:42:31 -0500

    footer done

--- vue/src/components/SiteFooter.vue
+++ vue/src/components/SiteFooter.vue
@@ -1,23 +1,29 @@
 <template>
-    <footer class="footer lexend-header-font">
-      <div class="container">
+    <div class="footer">
+      <!-- <div class="container"> -->
+        <div class="addresscontainer">
         <div class="addresscr">
         <p class="copyright">Copyright &copy; {{ new Date().getFullYear() }} Sprout</p>
         <p class="address">1275 Kinnear Rd #121, Columbus, OH 43212</p>
-        <p class="bi bi-envelope"> Contact us</p>
+        
+        </div>
         </div>
         <img class="logo" src="https://res.cloudinary.com/dwdijh29x/image/upload/v1733323940/sprout-icon-light_wvq61u.png" />
       <div class="abouticons">
-        <a class="aboutlink">About</a>
+        <div class="about-contact">
+        <a class="aboutlink" href="">About Us</a>
+        <a class="bi bi-envelope" href=""> Contact us</a>
+      </div>
         <div class="icons">
-          <p>Follow Us</p>
+          <p id="follow-us">Follow Us</p>
           <i class="bi bi-facebook"></i>
           <i class="bi bi-instagram"></i>
           <i class="bi bi-twitter-x"></i>
       </div>
+      
+      <!-- </div> -->
       </div>
-      </div>
-    </footer>
+    </div>
 </template>
 
 
@@ -27,18 +33,43 @@
 
 
 <style scoped>
-.container {
-  display: flex;
-  color: aliceblue;
 
- 
+#follow-us {
+  margin-top: 6px;
 }
+
 .icons{
 display: flex;
 justify-content: space-evenly;
-width: 130%;
+width: 100%;
+margin-right: 60px;
+margin-top: 15px;
 
 }
+
+.about-contact {
+  display: flex;
+  flex-direction: row;
+  width: 200px;
+  justify-content: space-evenly;
+  margin-right: 60px;
+}
+.abouticons {
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+  }
+  .bi-facebook{
+    font-size: x-large;
+  }
+  .bi-instagram{
+    font-size: x-large;
+  }
+  .bi-twitter-x{
+    font-size: x-large;
+  }
 
 .bi-facebook:hover {
   color: darkgreen;
@@ -56,10 +87,13 @@ color: darkgreen;
 transform: scale(1.5);
 transition: all 0.3s ease;
         }
-.bi-envelope:hover {
-color: darkgreen;
 
-        }
+
+.bi-envelope {
+  color: white;
+  size: 19px;
+  text-decoration: none;
+}
 
 .instagram{
   width: 50%;
@@ -74,30 +108,41 @@ color: darkgreen;
 .logo{
   display: block;
   margin-left: auto;
-  margin-right: auto;
+  margin-right: 35%;
   width: 70px;
 }
 
 .aboutlink{
   color: white;
   text-decoration: none;
-  justify-content: end;
-  font-size: 19px;
+  margin-right: 25px;
 }
 .footer {
+  display: flex;
+  flex-direction: row;
   padding: 10px;
   background-color: #679436;
+  width: 100%;
+  color: white;
+  /* justify-content: space-evenly; */
 }
-.lexend-header-font {
+/* .lexend-header-font {
   font-family: "Lexend", sans-serif;
   font-optical-sizing: auto;
   font-weight: 300;
   font-style: normal;
-}
+  
+} */
 .addresscr{
   display: flex;
   flex-direction: column;
   
+}
+.addresscontainer{
+  display: flex;
+  justify-content: left;
+/* width: 60%; */
+  margin-left: 70px;
 }
 
 </style>
