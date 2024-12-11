df3db2e392927b4332fef20ec5872e507920fdfe
Eliza Adams <elizaa29.ea@gmail.com>
2024-12-11 14:25:28 -0500

    more homepage

--- vue/src/components/SiteFooter.vue
+++ vue/src/components/SiteFooter.vue
@@ -4,11 +4,13 @@
         <div class="addresscr">
         <p class="copyright">Copyright &copy; {{ new Date().getFullYear() }} Sprout</p>
         <p class="address">1275 Kinnear Rd #121, Columbus, OH 43212</p>
+        <p class="bi bi-envelope"> Contact us</p>
         </div>
         <img class="logo" src="https://res.cloudinary.com/dwdijh29x/image/upload/v1733323940/sprout-icon-light_wvq61u.png" />
       <div class="abouticons">
-        <a class="aboutlink">About us</a>
+        <a class="aboutlink">About</a>
         <div class="icons">
+          <p>Follow Us</p>
           <i class="bi bi-facebook"></i>
           <i class="bi bi-instagram"></i>
           <i class="bi bi-twitter-x"></i>
@@ -37,15 +39,31 @@ justify-content: space-evenly;
 width: 130%;
 
 }
-.facebook{
-  width: 20%;
-  height: 20%;
-  
-  
-}
+
+.bi-facebook:hover {
+  color: darkgreen;
+  transform: scale(1.5);
+  transition: all 0.3s ease;
+        }
+  .bi-twitter-x:hover:hover {
+  color: darkgreen;
+  transform: scale(1.5);
+  transition: all 0.3s ease;
+        }
+
+.bi-instagram:hover {
+color: darkgreen;
+transform: scale(1.5);
+transition: all 0.3s ease;
+        }
+.bi-envelope:hover {
+color: darkgreen;
+
+        }
+
 .instagram{
-  width: 20%;
-  height: 20%;
+  width: 50%;
+  height: 50%;
 }
 .twitter{
   width: 20%;
@@ -64,7 +82,7 @@ width: 130%;
   color: white;
   text-decoration: none;
   justify-content: end;
-  font-size: large;
+  font-size: 19px;
 }
 .footer {
   padding: 10px;
