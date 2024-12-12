553a315f69afe90490f0d7d971567fd0049f5a5d
Joshua Hardy <jhardybuis@gmail.com>
2024-12-11 21:51:19 -0500

    RevertedSomeChangesOverwrittenDuringMergeConflict

--- vue/src/components/MyGarden.vue
+++ vue/src/components/MyGarden.vue
@@ -13,7 +13,7 @@
           <img class="plant-tab-image" :src="gardenPlant.plant.plantImage1" :alt="gardenPlant.plant.plantName" />
           <div class="plant-tab-name">{{ gardenPlant.plant.plantName }}</div>
         </div>
-        <div class="plant-tab" v-on:click="goToSearch()">
+        <div class="plant-tab" v-on:click="pushToSearch()">
           <i class="plant-tab-image bi bi-plus-square" style="color:gray; display:flex; font-size: 2.5rem "></i> <span
             class="plant-tab-name" style="color:gray">Add a Plant</span>
         </div>
@@ -166,9 +166,7 @@ export default {
     removePlant(id) {
       PlantService.removePlant(id);
       this.gardenPlants = this.gardenPlants.filter((gardenPlant) => gardenPlant.plant.plantId !== id);
-      if (this.selectedPlant && this.selectedPlant.plantId === id) {
-        this.selectedPlant = null;
-      }
+      this.selectedPlant = '';
       this.dropdownVisible = false;
     },
     startEditingQuantity() {
@@ -195,9 +193,7 @@ export default {
       PlantService.updatePlant(this.selectedPlant);
       this.isEditingNotes = false;
     },
-    goToSearch() {
-      this.$router.push({ name: "plantSearch" });
-    },
+
     goBack() {
       this.$router.go(-1);
     },
